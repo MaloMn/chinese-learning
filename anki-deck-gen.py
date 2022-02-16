24f89a0df9ff4db9f19830110ba9864f3cd67e23
malomn <malo.maisonneuve@gmail.com>
2022-02-16 11:09:38 +0100

    Re-added db, generated first svg characters

--- anki-deck-gen.py
+++ anki-deck-gen.py
@@ -16,7 +16,25 @@ def filter_characters_from_file(file_name, characters):
                     output.append(parsed)
             except NameError:
                 pass
+            except SyntaxError:
+                pass
     return output
+
+
+def generate_svg(graphics, character):
+    data = next(filter(lambda x: x["character"] == character, graphics))
+
+    output = "<svg viewBox=\"0 0 1024 1024\"><g transform=\"scale(1, -1) translate(0, -900)\">"
+    for g in data["strokes"]:
+        output += "<path d=\"" + g + "\"></path>"
+    output += "</g></svg>"
+
+    return output
+
+
+def save_svg(filename, svg):
+    with open(filename, "w+") as f:
+        f.write(svg)
 
 
 characters = ["我", "你"]
@@ -24,6 +42,9 @@ characters = ["我", "你"]
 # Reading lexical data
 dict_data = filter_characters_from_file("initial-db/dictionary.txt", characters)
 graph_data = filter_characters_from_file("initial-db/graphics.txt", characters)
+
+for c in characters:
+    save_svg("initial-db/svg/" + c + ".svg", generate_svg(graph_data, c))
 
 """
 Needs a custom model id :
