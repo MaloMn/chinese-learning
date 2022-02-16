a4841d21293489c16151d92487a0e96770aa62e9
malomn <malo.maisonneuve@gmail.com>
2022-02-16 10:06:14 +0100

    Added data from makemehanzi

--- anki-deck-gen.py
+++ anki-deck-gen.py
@@ -5,11 +5,33 @@ wo and ni.
 
 import genanki
 
+
+def filter_characters_from_file(file_name, characters):
+    output = []
+    with open(file_name, "r") as f:
+        for line in f.readlines():
+            try:
+                parsed = eval(line)
+                if parsed["character"] in characters:
+                    output.append(parsed)
+            except NameError:
+                pass
+    return output
+
+
+characters = ["我", "你"]
+
+# Reading lexical data
+dict_data = filter_characters_from_file("initial-db/dictionary.txt", characters)
+graph_data = filter_characters_from_file("initial-db/graphics.txt", characters)
+
 """
 Needs a custom model id :
 import random; random.randrange(1 << 30, 1 << 31)
 """
 
+
+"""
 main_model = genanki.Model(
   1463873326,
   'Chinese PinYin Audio',
@@ -24,3 +46,4 @@ main_model = genanki.Model(
       'afmt': '{{FrontSide}}<hr id="answer">{{Answer}}',
     },
   ])
+"""
