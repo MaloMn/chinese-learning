9c1446232fb02af1af090db9083c15c4f520395d
malomn <malo.maisonneuve@gmail.com>
2022-02-18 21:21:28 +0100

    Loaded necessary data in py file

--- anki-deck-gen.py
+++ anki-deck-gen.py
@@ -4,6 +4,13 @@ wo and ni.
 """
 
 import genanki
+import json
+
+characters = ["我", "你"]
+
+"""
+Generate SVG files for wanted characters
+"""
 
 
 def filter_characters_from_file(file_name, characters):
@@ -24,9 +31,9 @@ def filter_characters_from_file(file_name, characters):
 def generate_svg(graphics, character):
     data = next(filter(lambda x: x["character"] == character, graphics))
 
-    output = "<svg viewBox=\"0 0 1024 1024\"><g transform=\"scale(1, -1) translate(0, -900)\">"
+    output = "<svg viewBox=\"0 0 1024 1024\"><g color=\"white\" transform=\"scale(1, -1) translate(0, -900)\">"
     for g in data["strokes"]:
-        output += "<path d=\"" + g + "\"></path>"
+        output += "<path fill=\"currentcolor\" d=\"" + g + "\"></path>"
     output += "</g></svg>"
 
     return output
@@ -37,22 +44,33 @@ def save_svg(filename, svg):
         f.write(svg)
 
 
-characters = ["我", "你"]
-
+"""
 # Reading lexical data
 dict_data = filter_characters_from_file("initial-db/dictionary.txt", characters)
 graph_data = filter_characters_from_file("initial-db/graphics.txt", characters)
 
 for c in characters:
     save_svg("initial-db/svg/" + c + ".svg", generate_svg(graph_data, c))
+"""
+
+images = {c: "initial-db/svg/" + c + ".svg" for c in characters}
+
+"""
+Find corresponding audio files, pinyins & translations
+"""
+
+with open("initial-db/output.json", "r") as f:
+    data_audios = json.load(f)
+
+audios = {c: "initial-db/deck/" + data_audios[c][2] for c in characters}
+pinyins = {c: data_audios[c][0] for c in characters}
+translations = {c: data_audios[c][1] for c in characters}
 
 """
 Needs a custom model id :
 import random; random.randrange(1 << 30, 1 << 31)
 """
 
-
-"""
 main_model = genanki.Model(
   1463873326,
   'Chinese PinYin Audio',
@@ -67,4 +85,3 @@ main_model = genanki.Model(
       'afmt': '{{FrontSide}}<hr id="answer">{{Answer}}',
     },
   ])
-"""
